b53d160851552e26d0c7999b30485736816eb510
Vinoth Kumar Madurai <vinoth_kumarm@hotmail.com>
2023-04-08 03:50:05 -0600

    added new files

---
+++ src/aws-exports.ts
@@ -0,0 +1,8 @@
+const awsmobile = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_dFMrKMsrv',
+  aws_user_pools_web_client_id: '77u2fsar1778pbm8m4h777tevb',
+};
+
+export default awsmobile;
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://0zzrwfuki3.execute-api.us-east-1.amazonaws.com/prod',
+};
